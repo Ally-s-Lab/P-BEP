e02290fe89928eeba8b01e868bb7c864c5b2a54b
Guy Shur <guyshur@gmail.com>
2020-10-17 16:38:35 +0300

    added recreating training file script

---
+++ combine_training_file.sh
@@ -0,0 +1,7 @@
+cat lib/training1.csv >> lib/training.csv
+cat lib/training2.csv >> lib/training.csv
+cat lib/training3.csv >> lib/training.csv
+cat lib/training4.csv >> lib/training.csv
+cat lib/training5.csv >> lib/training.csv
+cat lib/training6.csv >> lib/training.csv
+cat lib/training7.csv >> lib/training.csv
